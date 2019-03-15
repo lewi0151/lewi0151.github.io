28753e1e15da78960117b2bd94237dec005b68da
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-03-15 00:39:19 -0500

    Update Project.js

--- Project.js
+++ Project.js
@@ -2,26 +2,6 @@ function Init() {
     fadeDuration: 600;
 }
 
-var url= "https://lewi0151.github.io/array.txt";
- var xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-  function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += arr[i].url + arr[i].display;
-    }
-    document.getElementById("id01").innerHTML = out;
-});
 function openModal() {
   document.getElementById('images').style.display = "block";
 }
