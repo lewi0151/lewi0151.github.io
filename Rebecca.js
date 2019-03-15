f1682b7602a0d3da1dd9fe6ffca74cc9d0762362
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-03-15 09:14:43 -0500

    Update Rebecca.js

--- Rebecca.js
+++ Rebecca.js
@@ -48,7 +48,7 @@ function translate()
     	var request = new XMLHttpRequest();
         textAPI = document.body.value;
         langAPI = document.getElementById("lang").value;
-		alert(textAPI!);
+		alert(textAPI);
         data = "key="+keyAPI+"&text="+textAPI+"&lang="+langAPI;
     request.open("POST",url,true);
     request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
