3f3e2609386419d63529f0324d55ef54552f0f22
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-03-14 19:16:41 -0500

    Update Rebecca.js

--- Rebecca.js
+++ Rebecca.js
@@ -43,7 +43,7 @@ function translate()
     	var keyAPI= "trnsl.1.1.20190311T054604Z.129c310cdc871e0d.34e1d905d9ed92739e3f50f56a8479f791e79363";
 	 var url = "https://translate.yandex.net/api/v1.5/tr.json/translate",
 
-	document.querySelector('#translate2').addEventListener('click', function() {
+	document.querySelector("#translate2")addEventListener('click', function() {
     	var request = new XMLHttpRequest(),
         textAPI = document.querySelector('#all').value,
         langAPI = document.querySelector('#lang').value
@@ -68,40 +68,5 @@ function translate()
 						    false);
 }
 
- function Lightbox()  {
-    var image = this;
-    $('body').on('click', 'a[rel^=lightbox], area[rel^=lightbox], a[data-lightbox], area[data-lightbox]', function(event) {
-      self.start($(event.currentTarget));
-      return false;
-    });
-  
-      if (typeof this.album[this.currentImageIndex].title !== 'undefined' &&
-      this.album[this.currentImageIndex].title !== '') {
-      var $caption = this.$lightbox.find('.lb-caption');
-      if (this.options.sanitizeTitle) {
-        $caption.text(this.album[this.currentImageIndex].title);
-      } else {
-        $caption.html(this.album[this.currentImageIndex].title);
-      }
-      $caption.fadeIn('fast')
-        .find('a').on('click', function(event) {
-          if ($(this).attr('target') !== undefined) {
-            window.open($(this).attr('href'), $(this).attr('target'));
-          } else {
-            location.href = $(this).attr('href');
-          }
-        })
-    }
-};
-   function LightboxNeighboringImages() {
-    if (this.album.length > this.currentImageIndex + 1) {
-      var preloadNext = new Image();
-      preloadNext.src = this.album[this.currentImageIndex + 1].link;
-    }
-    if (this.currentImageIndex > 0) {
-      var preloadPrev = new Image();
-      preloadPrev.src = this.album[this.currentImageIndex - 1].link;
-    }
-  };
-  
+
 
