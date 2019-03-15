3328a6614f8683f2814308fc34b021275cbe3cbd
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-03-15 08:32:41 -0500

    Update Project.js

--- Project.js
+++ Project.js
@@ -29,12 +29,12 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > 6) {slideIndex = 1}
-  if (n < 1) {slideIndex = 6}
-  for (i = 0; i < 4; i++) {
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 5; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
