ac6ee5999b8880a386d57659da1049f00eafe871
lewi0151 <45886861+lewi0151@users.noreply.github.com>
2019-03-15 02:27:27 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -26,12 +26,12 @@ function Init() {
 		client.get(url, function(response)
 		{
 		var response2=JSON.parse(response);
-	        document.getElementById("Mail").innerHTML=response2[0].Mail;
-	        document.getElementById("Objective").innerHTML=response2[0].Objective;
-	       	document.getElementById("College").innerHTML=response2[0].College1+response2[0].Date1+response2[0].College2+response2[0].Date2;
-		document.getElementById("Majors").innerHTML=response2[0].Major1+response2[0].Major2+response2[0].Minor1;
-		document.getElementById("Work Experience").innerHTML=response2[0].Job1+response2[0].Job2;
-	        document.getElementById("Skill").innerHTML=response2[0].Skill1+response2[0].Skill2+response2[0].Skill3;
-	        document.getElementById("Clubs").innerHTML=response2[0].Club1+response2[0].Club2+response2[0].Club3+response2[0].Club4+response2[0].Club5;
+	        document.getElementById("Mail").innerHTML=response2.Mail;
+	        document.getElementById("Objective").innerHTML=response2.Objective;
+	       	document.getElementById("College").innerHTML=response2.College1+response2.Date1+response2.College2+response2.Date2;
+		document.getElementById("Majors").innerHTML=response2.Major1+response2.Major2+response2.Minor1;
+		document.getElementById("Work Experience").innerHTML=response2.Job1+response2.Job2;
+	        document.getElementById("Skill").innerHTML=response2.Skill1+response2.Skill2+response2.Skill3;
+	        document.getElementById("Clubs").innerHTML=response2.Club1+response2.Club2+response2.Club3+response2.Club4+response2.Club5;
 		});
 		
